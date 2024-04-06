1b95572cbc342a09713102d45bb8f0b7659e03e2
Aleksey Bondarenko <alioshenkaia@yandex.ru>
2024-04-06 23:25:49 +0300

    Update

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -23,7 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('pages/', include('pages.urls')),
-    path('__debug__/', include('debug_toolbar.urls')),
     path('auth/', include('django.contrib.auth.urls')),
     path(
         'auth/registration/',
